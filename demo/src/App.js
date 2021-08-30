868a791fb15d3360d8a1a2b9cd2e090adb52399a
lioeaet <lioeaet@gmail.com>
2021-08-30 15:59:55 +0300

    wip

--- demo/src/App.js
+++ demo/src/App.js
@@ -7,7 +7,10 @@ const baseOrm = orm(() => ({
 
 const childOrm = orm(() => ({
   childBase: baseOrm,
-  childBaseArr: [baseOrm]
+  childBaseArr: [baseOrm],
+  inner: {
+    another: baseOrm
+  }
 }), 'childOrm')
 
 const base = {
@@ -24,18 +27,26 @@ const child = {
     id: 1,
     basePropY: 'y'
   },
-  childBaseArr: [{ id: 1, baseProp: 'x' }]
+  // childBaseArr: [{ id: 1, baseProp: 'x' }],
+  // inner: {
+  //   another: {
+  //     id: 1,
+  //     baseInner: 'in'
+  //   }
+  // }
 }
+// const child_2 = {
+//   childBase: []
+// }
 
-baseOrm.put(base.id, base)
-
-// console.log(baseOrm.get(base.id))
+// baseOrm.put(base.id, base)
+// baseOrm.put(base.id, )
 
 childOrm.put(child.id, child)
+childOrm.put(child.id, { childBase: { id: 1, prop: 'someZ' } })
+// childOrm.put(child.id, child_2)
 
-console.log(baseOrm.get(base.id))
 console.log(childOrm.get(child.id))
-
 
 // a: { b: { c: { prop1, prop2 } } }
 // UPDATE GRANDPA
