8d53bece18b84e8101a7e7012f12d52c9bf65809
lioeaet <lioeaet@gmail.com>
2021-09-01 12:43:09 +0300

    wip

--- src/utils/relations.js
+++ src/utils/relations.js
@@ -3,10 +3,11 @@ import { pathDecrement } from '*/utils/pathObj'
 
 export const theCount = Symbol('theCount')
 export const theEnd = Symbol('theEnd')
-console.log(g.items)
+
 export const relationsIncrement = (childNormId, parentNormId, stack) => {
   if (!parentNormId) return
   let pathToChild
+  const start = stack.indexOf(parentNormId) + 1
   // can't use just stack because parent can contain several childs 
   // ...and first path will rewrited by second
   if (!g.graph[childNormId]) {
@@ -17,10 +18,14 @@ export const relationsIncrement = (childNormId, parentNormId, stack) => {
   }
   else {
     pathToChild = g.graph[childNormId][parentNormId]
+    for (let i = start; i < stack.length; i++) {
+      const key = stack[i]
+      if (pathToChild[key] === theEnd) return
+      if (!pathToChild[key]) break
+    }
     pathToChild[theCount]++
   }
 
-  const start = stack.indexOf(parentNormId) + 1
   for (let i = start; i < stack.length; i++) {
     const key = stack[i]
     if (i === stack.length - 1) pathToChild[key] = theEnd
@@ -32,16 +37,27 @@ export const relationsIncrement = (childNormId, parentNormId, stack) => {
   }
 }
 
-export const relationsDecrement = (childNormId, parentNormId) => {
+export const relationsDecrement = (childNormId, parentNormId, stack) => {
   if (!parentNormId) return
-  pathDecrement(g.parents, childNormId, parentNormId)
+
+  let current = g.graph[childNormId][parentNormId]
+  const start = stack.indexOf(parentNormId) + 1
+  let i = start
+
+  while(current[theCount] !== 1) {
+    current[theCount]--
+    current = current[stack[i++]]
+  }
+  if (i === start) delete g.graph[parentNormId]
+  else delete current[stack[i]]
 }
 
 export const relationsUpdateArrayRemovedChilds = (
   prevArray,
   nextArray,
   nextChilds,
-  parentNormId
+  parentNormId,
+  stack
 ) => {
   const prevChilds = g.arrayChilds.get(prevArray)
   g.arrayChilds.set(nextArray, nextChilds)
@@ -49,6 +65,6 @@ export const relationsUpdateArrayRemovedChilds = (
 
   for (let normId of prevChilds.keys()) {
     if (nextChilds.has(normId)) continue
-    relationsDecrement(normId, parentNormId)
+    relationsDecrement(normId, parentNormId, stack)
   }
 }
